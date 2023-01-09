51c3d238ae211ffc0c0300dab006556858dcb053
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-09 23:23:23 +0100

    checked

---
+++ Day-1/band_name_generator.py
@@ -0,0 +1,4 @@
+message = "Welcome to the Band Name Generator!"
+city = input("What city did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name should be", city, pet)
