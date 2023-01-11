c3944146be5ea4be07addcbc798a41f315d4f0c1
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-11 17:12:40 +0100

    Update exercise_5.py

--- Day-3/exercise_5.py
+++ Day-3/exercise_5.py
@@ -1,22 +1,27 @@
+
+# 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
 
-name = name1 + name2
-name = name.lower()
-t = name.count("t")
-r = name.count("r")
-u = name.count("u")
-e = name.count("e")
+#Write your code below this line 👇
+all_names = (name1 + name2).lower()
+t = all_names.count("t")
+r = all_names.count("r")
+u = all_names.count("u")
+e = all_names.count("e")
+l = all_names.count("l")
+o = all_names.count("o")
+v = all_names.count("v")
+
 true = t + r + u + e
-l = name.count("l")
-o = name.count("o")
-v = name.count("v")
 love = l + o + v + e
-score = int(true + love)
+score = (true + 10) + love
+
 if (score < 10) or (score > 90):
   print("Your score is {}, you go together like coke and mentos.".format(score))
-elif (loveInt <= 50) and (loveInt >= 40):
+elif (score >= 40) and (score <= 50):
   print("Your score is {}, you are alright together.".format(score))
 else:
-  print("Your score is {}".format(score))
+  print("Your score is {}.".format(score))
