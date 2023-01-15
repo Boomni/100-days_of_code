afc2e90e0453fda0323e5d3f2d8dedb46cea4042
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-15 17:15:50 +0100

    Update exercise-1.py

--- Day-2/exercise-1.py
+++ Day-2/exercise-1.py
@@ -1,21 +1,6 @@
-"""
-Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
 
-Warning. Your program should work for different inputs. e.g. any two-digit number.
-
-Example Input
-39
-Example Output
-3 + 9 = 12
-12
-"""
-
-# 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
-# 🚨 Don't change the code above 👆
 
-
-#Write your code below this line 👇
 x = int(two_digit_number[0])
 y = int(two_digit_number[1])
 
