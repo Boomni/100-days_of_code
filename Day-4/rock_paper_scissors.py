c5c6dc13002096a0bc914448add9f93bfa927fb5
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-15 22:41:29 +0100

    edit

--- Day-4/rock_paper_scissors.py
+++ Day-4/rock_paper_scissors.py
@@ -29,6 +29,17 @@ scissors = '''
 
 import random
 
+rock_paper_scissors = """
+        Rock                              Paper                          Scissors
+        _______                        ________                         _______    
+    ---'   ____)                   ---'    ____)____                ---'   ____)____
+          (_____)                             ______)                         ______)
+          (_____)                             _______)                     __________)
+          (____)                             _______)                     (____)
+    ---.__(___)                    ---.___________)                 ---.__(___)
+"""
+print("\n{}\n{}{}".format("Welcome".center(90), "To".center(90), rock_paper_scissors))
+
 choice = [rock, paper, scissors]
 
 comp_choice = random.randint(0, 2)
@@ -37,24 +48,24 @@ computer = choice[comp_choice]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. :"))
 user = choice[user_choice]
 
-draw = "Computer chose {}, you chose {} >>> It's a Draw!"
-win = "Computer chose {}, you chose {} >>> You Win!"
-loose = "Computer chose {}, you chose {} >>> You Loose!"
+draw = "Computer chose {}\nYou chose {}{}"
+win = "Computer chose {}\nYou chose {}{}"
+loose = "Computer chose {}\nYou chose {}{}"
 
 if comp_choice == user_choice:
-    print(draw.format(computer, user))
+    print(draw.format(computer, user, "Its a Draw!".center(90)))
 elif comp_choice == 0 and user_choice == 2:
-    print(loose.format(computer, user))
+    print(loose.format(computer, user, "You Loose!".center(90)))
 elif comp_choice == 0 and user_choice == 1:
-    print(win.format(computer, user))
+    print(win.format(computer, user, "You Win!".center(90)))
 elif comp_choice == 2 and user_choice == 1:
-    print(loose.format(computer, user))
+    print(loose.format(computer, user, "You Loose!".center(90)))
 elif comp_choice == 2 and user_choice == 0:
-    print(win.format(computer, user))
+    print(win.format(computer, user, "You Win!".center(90)))
 elif comp_choice == 1 and user_choice == 2:
-    print(win.format(computer, user))
+    print(win.format(computer, user, "You Win!".center(90)))
 elif comp_choice == 1 and user_choice == 0:
-    print(loose.format(computer, user))
+    print(loose.format(computer, user, "You Loose".center(90)))
 
 print()
-print("Game Over!")
+print("Game Over!".center(90))
