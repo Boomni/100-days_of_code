4d5c540fa0a955f992f8917536349e6cb798d058
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-17 00:49:00 +0100

    Create hangman.py

---
+++ Day-7/hangman.py
@@ -0,0 +1,60 @@
+import random
+
+from hangman_words import word_list
+
+chosen = random.choice(word_list).lower()
+length = len(chosen)
+
+end = False
+lives = 6
+
+from hangman_art import logo
+
+print(logo)
+instructions = """
+    Instructions:
+        A word is chosen at random.
+        Make a guess of each letter, to guess the word.
+        Wrong guess, you'll be hung!.
+        Good luck! :)
+"""
+print(instructions)
+
+display = []
+for _ in range(length):
+    display.append("_")
+
+while not end:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print("You've already guessed", guess)
+
+    #Check guessed letter
+    for position in range(length):
+        letter = chosen[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen:
+        print("You guessed", guess, "that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end = True
+            
+    print(f"{' '.join(display).upper()}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end = True
+    
+    from hangman_art import stages
+    print(stages[lives])
+
+if lives == 0:
+        print("The word was", chosen.upper())
+        print("You lose!")
+if "_" not in display:
+        print("You win!")
+print("Game Over!")
