14c1d4b70e65c5a72bbdba5f267c048bba1bc0eb
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-17 01:03:35 +0100

    Create hangman_instruction.py

---
+++ Day-7/hangman_instruction.py
@@ -0,0 +1,7 @@
+instructions = """
+    Instructions:
+        A word is chosen at random.
+        Make a guess of each letter, to guess the word.
+        Wrong guess, you'll be hung!.
+        Good luck! :)
+"""
