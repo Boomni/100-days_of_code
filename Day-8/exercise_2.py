81518e2b7bc594fb6fc58de9756aa9d5452ec706
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-17 03:27:09 +0100

    Create exercise_2.py

---
+++ Day-8/exercise_2.py
@@ -0,0 +1,17 @@
+def prime_checker(number = 0):
+    numbers = []
+    for i in range(1, number + 1):
+        numbers.append(i)
+   
+    divisors = []
+    for i in numbers:
+        if number % i == 0:
+            divisors.append(i)
+
+    if len(divisors) == 2:
+        print("It's a Prime Number")
+    else:
+        print("Not a Prime Number")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
