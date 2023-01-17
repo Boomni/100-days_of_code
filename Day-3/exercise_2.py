7635a8beb324b1a489b8ae78786b57944a3c3ba0
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-17 21:52:56 +0100

    Update exercise_2.py

--- Day-3/exercise_2.py
+++ Day-3/exercise_2.py
@@ -1,22 +1,6 @@
-"""
-Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
-
-It should tell them the interpretation of their BMI based on the BMI value.
-
-Example Input
-weight = 85
-height = 1.75
-Example Output
-85 ÷ (1.75 x 1.75) = 27.755102040816325
-
-Your BMI is 28, you are slightly overweight.
-"""
-# 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 bmi = round(weight / (height ** 2))
 if bmi < 18.5:
     print("Your BMI is {}, you are underweight.".format(bmi))
